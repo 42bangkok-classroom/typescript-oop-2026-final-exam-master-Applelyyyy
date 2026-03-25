e9671c661896af20875dca8484b544ce2d69bc55
Applelyyyy <islandgameming@gmail.com>
2026-03-25 11:52:01 +0700

    FFF

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import path from 'path';
 import * as fs from 'fs';
 import { Products } from './products_interface/interface_product';
+import { ApiResponse } from 'src/interfaces/response.interface';
 @Injectable()
 export class ProductsService {
   private filePath = path.join(__dirname, '../../data/products.json');
@@ -11,7 +12,7 @@ export class ProductsService {
     const data = fs.readFileSync(this.filePath, 'utf-8');
     return data ? JSON.parse(data) : [];
   }
-  findAll() :Products[] {
+  findAll(): ApiResponse<Products[]> {
     return this.readFile();
   }
 }
