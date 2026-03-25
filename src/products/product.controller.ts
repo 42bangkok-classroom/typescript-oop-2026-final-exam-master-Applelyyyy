a8748b9fff4b456a9ecb18c0df62e765a3b36b25
Applelyyyy <islandgameming@gmail.com>
2026-03-25 12:12:31 +0700

    AA

--- src/products/product.controller.ts
+++ src/products/product.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { ProductsService } from './product.service';
+import type { ApiResponse } from 'src/interfaces/response.interface';
+import { Products } from './products_interface/interface_product';
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
   @Get()
-  findAll() {
+  findAll(): ApiResponse<Products[]> {
     return {
       success: true,
       data: this.productsService.findAll(),
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -2,12 +2,11 @@ import { Injectable } from '@nestjs/common';
 import path from 'path';
 import * as fs from 'fs';
 import { Products } from './products_interface/interface_product';
-import { ApiResponse } from 'src/interfaces/response.interface';
 @Injectable()
 export class ProductsService {
   private filePath = path.join(__dirname, '../../data/products.json');
 
-    findAll() : ApiResponse<Products[]> {
+  findAll(): Products[] {
     const data = fs.readFileSync(this.filePath, 'utf-8');
     const data_final = JSON.parse(data);
     return data_final;
